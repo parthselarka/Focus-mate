d25a84df17e0a262e5d877e839479f8d6035acb6
parthselarka <116080750+parthselarka@users.noreply.github.com>
2024-11-05 18:54:23 +0530

    third update schedule.js

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -3,8 +3,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const calendarEl = document.getElementById('calendar');
     const calendar = new FullCalendar.Calendar(calendarEl, {
         initialView: 'timeGridWeek',
-        events: '/api/tasks',
-        locale: 'eng',
+        locale: 'en', // Corrected from 'eng' to 'en'
+        timeZone: 'Asia/Kolkata', // Set the timezone to IST
 
         headerToolbar: {
             left: 'prev,next today',
@@ -13,7 +13,27 @@ document.addEventListener('DOMContentLoaded', function() {
         },
         selectable: true,
         editable: true,
-        
+
+        // Use the events function to fetch and adjust events
+        events: function(fetchInfo, successCallback, failureCallback) {
+            $.ajax({
+                url: '/api/tasks',
+                method: 'GET',
+                success: function(data) {
+                    const adjustedEvents = data.map(event => ({
+                        ...event,
+                        start: new Date(event.start).toLocaleString("en-IN", { timeZone: "Asia/Kolkata" }),
+                        end: new Date(event.end_event).toLocaleString("en-IN", { timeZone: "Asia/Kolkata" })
+                    }));
+                    successCallback(adjustedEvents);
+                },
+                error: function(error) {
+                    console.error('Error fetching events:', error);
+                    failureCallback(error);
+                }
+            });
+        },
+
         select: function(selectInfo) {
             const title = prompt('Event Title:');
             if (title) {
@@ -25,29 +45,29 @@ document.addEventListener('DOMContentLoaded', function() {
                         end: selectInfo.endStr, // This should be local time
                         allDay: selectInfo.allDay
                     };
-        
+
                     // Convert to UTC for sending to the server
                     const startUtc = new Date(eventData.start).toISOString();
                     const endUtc = new Date(eventData.end).toISOString();
-        
+
                     $.ajax({
                         url: '/api/tasks',
                         type: 'POST',
                         contentType: 'application/json',
                         data: JSON.stringify({
                             ...eventData,
-                            start: startUtc, // Send as UTC
-                            end: endUtc // Send as UTC
+                            start: startUtc, // Send as UTC to the server
+                            end: endUtc
                         }),
                         success: function(data) {
                             console.log('Event added successfully', data);
-                            
+                            // Add the event to the calendar using local time again for display
                             calendar.addEvent({
                                 ...data,
                                 id: data.task_id,
                                 title: data.title,
-                                start: data.start, // Keep local time for display
-                                end: data.end_event, // Keep local time for display
+                                start: eventData.start, // Keep local time for display
+                                end: eventData.end, // Keep local time for display
                                 allDay: data.all_day
                             });
                         },
@@ -58,11 +78,10 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             }
         },
-        
 
         eventDidMount: function(info) {
-            console.log("Event details:", info.event); 
-            console.log("Mounted event ID:", info.event.id); 
+            console.log("Event details:", info.event);
+            console.log("Mounted event ID:", info.event.id);
             info.el.addEventListener('contextmenu', function(e) {
                 e.preventDefault(); // Prevent the default context menu from showing
                 showContextMenu(e, info.event.id); // Pass the event id to the showContextMenu function
@@ -70,7 +89,7 @@ document.addEventListener('DOMContentLoaded', function() {
             if (info.event.extendedProps.completed) {
                 // Apply gray background color for completed tasks/events
                 info.el.style.backgroundColor = 'gray';
-                info.el.style.borderColor= 'gray';
+                info.el.style.borderColor = 'gray';
             }
         },
 
@@ -86,8 +105,6 @@ document.addEventListener('DOMContentLoaded', function() {
         },
     });
 
-    
-
     function showContextMenu(e, eventId) {
         e.preventDefault(); // Prevent the default context menu from showing
         currentEventId = eventId; // Store the ID of the event that was right-clicked
@@ -100,12 +117,10 @@ document.addEventListener('DOMContentLoaded', function() {
         contextMenu.style.top = `${e.pageY}px`;
     }
 
-    // Assuming deleteEvent function exists and handles event deletion
-    
     document.addEventListener('click', function(e) {
         const contextMenu = document.getElementById("contextMenu");
         const deleteButton = document.getElementById("deleteEvent");
-    
+
         // Check if the click is outside the context menu and not on the delete button
         if (!contextMenu.contains(e.target) && e.target !== deleteButton) {
             contextMenu.style.display = 'none'; // Hide the context menu
@@ -115,9 +130,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     document.getElementById("deleteEvent").addEventListener('click', function() {
         if (currentEventId !== null) {
-            
-            
-            fetch('api/tasks/' + parseInt(currentEventId) , {
+            fetch('api/tasks/' + parseInt(currentEventId), {
                 method: 'DELETE',
                 credentials: 'include',
             })
@@ -130,21 +143,19 @@ document.addEventListener('DOMContentLoaded', function() {
                 // Remove the event from FullCalendar
                 let event = calendar.getEventById(currentEventId);
                 if (event) event.remove();
-    
+
                 // Hide the context menu
                 document.getElementById("contextMenu").style.display = 'none';
                 currentEventId = null;
             })
             .catch(error => console.error('Error deleting event:', error));
         }
-                
     });
 
     function toUTCDate(dateString) {
         const localDate = new Date(dateString);
         return new Date(Date.UTC(localDate.getFullYear(), localDate.getMonth(), localDate.getDate(), localDate.getHours(), localDate.getMinutes(), localDate.getSeconds()));
     }
-    
 
     function updateEventInDatabase(event) {
         const eventId = event.id; // Use event.id which should correspond to task_id in your DB
@@ -152,7 +163,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const end = event.endStr; 
         const title = event.title; // Assuming you're getting this from somewhere
         const allDay = event.allDay; // Assuming this is part of your event object
-    
+
         console.log("Updating event ID:", eventId); // Add this line
         fetch('/api/tasks/' + eventId, {
             method: 'PUT',
@@ -180,8 +191,6 @@ document.addEventListener('DOMContentLoaded', function() {
             console.error('Error updating event:', error);
         });
     }
-    
-    
-    
+
     calendar.render();
 });
